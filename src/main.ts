9203649c4029373ef22f5f49d59901485b5bdd0a
nick3948 <nikhilgattu16@gmail.com>
2024-02-29 18:56:42 +0530

    added speed insights

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { AppModule } from './app/app.module';
+import { injectSpeedInsights } from '@vercel/speed-insights';
 
+injectSpeedInsights();
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.error(err));
